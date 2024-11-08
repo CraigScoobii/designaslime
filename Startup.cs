06d8a85e5ed1cdde847028f89785ead46dcbf281
CraigScoobii <craig.allan.smith.personal@gmail.com>
2024-11-08 12:46:27 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DesignASlime2024.Startup))]
+namespace DesignASlime2024
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
